c623d4b931b8ac42039349ff2d85fc9152f7a3c8
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-19 09:25:27 -0600

    Add uniqueness to User and add Messages to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
-  validates :username, presence: true, length: {minimum: 3, maximum: 25}
+  validates :username, presence: true, length: {minimum: 3, maximum: 25}, 
+            uniqueness: { case_sensitive: false }
+  has_many :messages
   has_secure_password
 
 end
