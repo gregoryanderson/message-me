daa2465e51a11827b8e39997c728f9428dd9f962
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-20 13:12:58 -0600

    Add users to routes and add controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'chatroom#index'
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
